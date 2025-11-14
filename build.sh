7a7fe187884a21fc25ac4497ce046d05ddad2284
Angeline Arrogante <delacruzangeline071905@gmail.com>
2025-11-14 21:29:40 +0800

    Add automatic migrations to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+set -o errexit  # exit if any command fails
+
+# Install dependencies
 pip install -r requirements.txt
-npm install
-npm run build:css
+
+# Run Django migrations
+python manage.py migrate --noinput
+
+# Collect static files
 python manage.py collectstatic --noinput
