a3bd11656e97ee240eaadc515b8125cdcc55a931
Angeline Arrogante <delacruzangeline071905@gmail.com>
2025-11-15 09:08:27 +0800

    Fix MEDIA_URL for Cloudinary

--- kakanin/models.py
+++ kakanin/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    preparation_days = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='kakanin_images/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class UserProfile(models.Model):
     # Naval, Biliran Barangays
     BARANGAY_CHOICES = [
